54761c9fc55b068de3a03755c90c61c6ba298e09
João Paulo <costajoaopaulo113@gmail.com>
2025-07-28 21:04:10 -0300

    feat: new images

--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -3,6 +3,10 @@ import vistas2 from "public/images/vistas2.jpg";
 import gringaEnglish from "public/images/gringa.png";
 import marcacao from "public/images/marcacao.jpg";
 import aeroportoVoo from "public/images/voos.png";
+import sghtmobile from "public/images/sght-mobile.png";
+import sght from "public/images/sght.png";
+import vistasmob from "public/images/mobile-vistas2.png";
+import vistas from "public/images/mobile-vistas.png";
 
 export const products = [
   {
@@ -10,8 +14,8 @@ export const products = [
     title: "Vistas Explodidas",
     description:
       "Vistas Explodidas é uma plataforma intuitiva para funcionários da empresa acessarem e compartilharem documentos importantes.",
-    thumbnail: vistasExplodidas,
-    images: [vistasExplodidas, vistas2],
+    thumbnail: vistasmob,
+    images: [vistasmob, vistasExplodidas, vistas, vistas2],
     stack: ["Vuejs", "Ant-Design", "Vuex", "Spring Boot", "PostgreSQL", "NGINX", "Docker"],
     slug: "aceternity",
     content: (
@@ -103,6 +107,36 @@ export const products = [
     thumbnail: aeroportoVoo,
     images: [aeroportoVoo],
     stack: ["Vuejs", "Ant-Design", "Vuex", "Nestjs", "PostgreSQL"],
+    slug: "tailwindmasterkit",
+    content: (
+      <div>
+        <p>
+          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
+          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
+          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
+          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
+          non cupidatat duis esse velit ut culpa et.{" "}
+        </p>
+        <p>
+          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
+          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
+          sit aute fugiat incididunt nostrud consequat proident fugiat id.
+          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
+          reprehenderit. Cillum Lorem veniam eu magna exercitation.
+          Reprehenderit adipisicing minim et officia enim et veniam Lorem
+          excepteur velit adipisicing et Lorem magna.
+        </p>{" "}
+      </div>
+    ),
+  },
+  {
+    href: "",
+    title: "SGHT",
+    description:
+      "Um sistema de gerenciamento de RECURSOS HUMANOS, nele os funcionário do RH podem administrar as horas extras solicitadas e horas válidas de cada funcionário.",
+    thumbnail: sghtmobile,
+    images: [sghtmobile, sght],
+    stack: ["Vuejs", "Ant-Design", "Vuex", "Spring Boot", "PostgreSQL", "NGINX", "Docker"],
     slug: "tailwindmasterkit",
     content: (
       <div>
